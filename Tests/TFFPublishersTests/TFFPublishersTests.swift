bb878204adcf195113e759f26b9f93078d77c04c
Guillaume Lessard <dhtnstff@gmail.com>
2020-07-21 18:00:19 -0600

    add tests for ConcatenateMany

--- Tests/TFFPublishersTests/TFFPublishersTests.swift
+++ Tests/TFFPublishersTests/TFFPublishersTests.swift
@@ -52,4 +52,57 @@ final class TFFPublishersTests: XCTestCase
     waitForExpectations(timeout: 1.0)
     c.cancel()
   }
+
+  func testConcatenateMany()
+  {
+    let i = Int.random(in: 10...100)
+    let p = (0..<i).reversed().map(Just.init)
+    let concatenated = ConcatenateMany(publishers: p).prefix(10)
+
+    let e = expectation(description: #function)
+    let c = concatenated.last().sink(
+      receiveCompletion: {
+        completion in
+        XCTAssertEqual(completion, .finished)
+        e.fulfill()
+      },
+      receiveValue: {
+        XCTAssertEqual($0, i-10)
+      }
+    )
+
+    waitForExpectations(timeout: 1.0)
+    c.cancel()
+  }
+
+  func testConcatenateWithFailures()
+  {
+    let i = Int.random(in: 1...100)
+    let p = (0...i).map {
+      j -> Result<Int, TestError>.Publisher in
+      let r: Result<Int, TestError>
+      if j < i
+      { r  = .success(j) }
+      else
+      { r = .failure(.value(j)) }
+      return r.publisher
+    }
+    let concatenated = ConcatenateMany(publishers: p)
+
+    let e = expectation(description: #function)
+    let c = concatenated.sink(
+      receiveCompletion: {
+        completion in
+        XCTAssertEqual(completion, .failure(.value(i)))
+        e.fulfill()
+      },
+      receiveValue: {
+        value in
+        XCTAssertLessThan(value, i)
+      }
+    )
+
+    waitForExpectations(timeout: 1.0)
+    c.cancel()
+  }
 }
